18037986a994e79ccf7d8ce96c0589d619b1e2c3
Chamath Ariyawansa <chamathariyawansa@gmail.com>
2018-10-05 13:24:27 +0530

    remove unnecessary components form the theme

--- IOTPlatformClient/src/app/layout/layout-routing.module.ts
+++ IOTPlatformClient/src/app/layout/layout-routing.module.ts
@@ -11,11 +11,11 @@ const routes: Routes = [
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'users', loadChildren: './users/users.module#UsersModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
+            // { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
+            // { path: 'forms', loadChildren: './form/form.module#FormModule' },
+            // { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
+            // { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
+            // { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
             { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
         ]
     }
