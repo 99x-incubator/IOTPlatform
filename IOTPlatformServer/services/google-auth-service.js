af30502e3733dadd030eaba3e8da9feb66962ee2
buddhi <buddhi.amigo@live.com>
2018-10-06 04:18:30 +0530

    Added configs to AWS param store

--- IOTPlatformServer/services/google-auth-service.js
+++ IOTPlatformServer/services/google-auth-service.js
@@ -6,15 +6,15 @@ const { credentials } = require('../configs/google/credentials');
 module.exports = {
 	getGoogleAuthClient: () => {
 		const oauth2Client = new OAuth2Client(
-			clientSecret.installed.client_id,
-			clientSecret.installed.client_secret,
-			clientSecret.installed.redirect_uris[1]
+			process.env.CLIENTID,
+			process.env.CLIENT_SECRET,
+			process.env.REDIRECT_URIS
 		);
 
 		oauth2Client.setCredentials({
-			access_token: credentials.access_token,
-			refresh_token: credentials.refresh_token,
-			expiry_date: credentials.expiry_date
+			access_token: process.env.ACCESS_TOKEN,
+			refresh_token: credentials.REFRESH_TOKEN,
+			expiry_date: credentials.EXPIRY_DATE
 		});
 
 		return oauth2Client;
