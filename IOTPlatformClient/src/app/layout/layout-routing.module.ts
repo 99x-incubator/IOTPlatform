38c34793432e251bee7d4eec39c68b722b572509
Namila Perera <NamilaP@99x.lk>
2018-10-05 20:19:46 +0530

    Subscription compenent added

--- IOTPlatformClient/src/app/layout/layout-routing.module.ts
+++ IOTPlatformClient/src/app/layout/layout-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'users', loadChildren: './users/users.module#UsersModule' },
             { path: 'topics', loadChildren: './topics/topics.module#TopicsModule' },
-            // { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+            { path: 'subscriptions', loadChildren: './subscriptions/subscriptions.module#SubscriptionsModule' },
             // { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
             // { path: 'forms', loadChildren: './form/form.module#FormModule' },
             // { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
